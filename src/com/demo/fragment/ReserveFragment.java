5e634a1d7adadeb5adb078c13afe6d54cdb171c5
gsqgit <506868617@qq.com>
2014-11-25 23:21:11 +0800

    reserve fragment-update

--- src/com/demo/fragment/ReserveFragment.java
+++ src/com/demo/fragment/ReserveFragment.java
@@ -18,7 +18,6 @@ import com.example.demo.R;
 public class ReserveFragment extends Fragment {
 	private Spinner equipmentGroup,currentStateGroup,timeGroup;
 	private ArrayAdapter<CharSequence> equipmentGroupAdapter,currentStateAdapter,timeGroupAdapter;
-	private SpinnerGroupListener spinnerListener;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -34,41 +33,15 @@ public class ReserveFragment extends Fragment {
 		currentStateGroup.setAdapter(currentStateAdapter);
 		timeGroupAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.timegroup, R.layout.spinner_text);
 		timeGroup.setAdapter(timeGroupAdapter);
-		spinnerListener = new SpinnerGroupListener();
-		equipmentGroup.setOnItemSelectedListener(spinnerListener);
-		currentStateGroup.setOnItemSelectedListener(spinnerListener);
-		timeGroup.setOnItemSelectedListener(spinnerListener);
+		equipmentGroup.setOnItemSelectedListener(new equipmentGroupListener());
+		currentStateGroup.setOnItemSelectedListener(new currentStateGroupListener());
+		timeGroup.setOnItemSelectedListener(new timeGroupListener());
 		return v;
 	}
 	
-	class SpinnerGroupListener implements OnItemSelectedListener{
-		@Override
-		public void onItemSelected(AdapterView<?> parent, View view,
-				int position, long id) {
-			// TODO Auto-generated method stub
-			switch(view.getId()){
-			case R.id.equipmentGroup:
-				Log.i("testout", "equipmentAdapter----------->"+equipmentGroupAdapter.getItem(position));
-				Toast.makeText(getActivity(), "您选择了：" + equipmentGroupAdapter.getItem(position), Toast.LENGTH_SHORT).show();
-				break;
-			case R.id.currentStateGroup:
-				
-				break;
-			case R.id.timeGroup:
-				
-				break;
-			}
-		}
-		@Override
-		public void onNothingSelected(AdapterView<?> parent) {
-			// TODO Auto-generated method stub
-			
-		}
-	}
+
 	
-	
-	
-/*	private class equipmentGroupListener implements OnItemSelectedListener{
+	private class equipmentGroupListener implements OnItemSelectedListener{
 		@Override
 		public void onItemSelected(AdapterView<?> parent, View view,
 				int position, long id) {
@@ -109,6 +82,31 @@ public class ReserveFragment extends Fragment {
 			// TODO Auto-generated method stub
 			
 		}
+	}
+	
+	/*	class SpinnerGroupListener implements OnItemSelectedListener{
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view,
+			int position, long id) {
+		// TODO Auto-generated method stub
+		switch(view.getId()){
+		case R.id.equipmentGroup:
+			Log.i("testout", "equipmentAdapter----------->"+equipmentGroupAdapter.getItem(position));
+			Toast.makeText(getActivity(), "您选择了：" + equipmentGroupAdapter.getItem(position), Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.currentStateGroup:
+			
+			break;
+		case R.id.timeGroup:
+			
+			break;
+		}
+	}
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
+		// TODO Auto-generated method stub
 		
-	}*/
+	}
+}*/
+
 }
